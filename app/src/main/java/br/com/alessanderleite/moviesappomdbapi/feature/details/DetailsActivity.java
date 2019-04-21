b7aed51246c388115bc7dfc859fe9a52b8a34479
alessanderleite <alessander.logistica@gmail.com>
2019-04-21 19:00:19 -0300

    create the details package and empty classes

---
+++ app/src/main/java/br/com/alessanderleite/moviesappomdbapi/feature/details/DetailsActivity.java
@@ -0,0 +1,15 @@
+package br.com.alessanderleite.moviesappomdbapi.feature.details;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.com.alessanderleite.moviesappomdbapi.R;
+
+public class DetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+    }
+}
---
+++ app/src/main/java/br/com/alessanderleite/moviesappomdbapi/feature/details/DetailsPresenter.java
@@ -0,0 +1,4 @@
+package br.com.alessanderleite.moviesappomdbapi.feature.details;
+
+public class DetailsPresenter {
+}
