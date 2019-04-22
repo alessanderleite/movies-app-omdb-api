8755845b78253b9ce80cb24516ddab70727b8b9b
alessanderleite <alessander.logistica@gmail.com>
2019-04-21 21:04:56 -0300

    code the class named Rating

--- app/src/main/java/br/com/alessanderleite/moviesappomdbapi/models/Rating.java
+++ app/src/main/java/br/com/alessanderleite/moviesappomdbapi/models/Rating.java
@@ -1,4 +1,57 @@
 package br.com.alessanderleite.moviesappomdbapi.models;
 
-public class Rating {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Rating implements Parcelable {
+    @SerializedName("Source")
+    private String source;
+    @SerializedName("Value")
+    private String value;
+
+    protected Rating(Parcel in) {
+        source = in.readString();
+        value = in.readString();
+    }
+
+    public static final Creator<Rating> CREATOR = new Creator<Rating>() {
+        @Override
+        public Rating createFromParcel(Parcel in) {
+            return new Rating(in);
+        }
+
+        @Override
+        public Rating[] newArray(int size) {
+            return new Rating[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(source);
+        dest.writeString(value);
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
 }
