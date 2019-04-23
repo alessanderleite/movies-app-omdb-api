04f918345306edf030b8ee3e5c117db186e6c233
alessanderleite <alessander.logistica@gmail.com>
2019-04-23 20:03:28 -0300

    code the BaseLayout.java class

--- app/src/main/java/br/com/alessanderleite/moviesappomdbapi/core/base/BaseLayout.java
+++ app/src/main/java/br/com/alessanderleite/moviesappomdbapi/core/base/BaseLayout.java
@@ -1,4 +1,30 @@
 package br.com.alessanderleite.moviesappomdbapi.core.base;
 
-public abstract class BaseLayout {
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+public abstract class BaseLayout extends LinearLayout {
+    public BaseLayout(Context context) {
+        super(context);
+        init(context);
+    }
+
+    public BaseLayout(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
+    }
+
+    public BaseLayout(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context);
+    }
+
+    public BaseLayout(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        init(context);
+    }
+
+    public abstract void init(Context context);
 }
