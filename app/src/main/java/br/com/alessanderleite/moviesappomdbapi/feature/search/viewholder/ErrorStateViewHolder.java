7e13f9cca75d399f4d69383719ef62a05b48e802
alessanderleite <alessander.logistica@gmail.com>
2019-04-27 19:40:47 -0300

    code the class named ErrorStateViewHolder

--- app/src/main/java/br/com/alessanderleite/moviesappomdbapi/feature/search/viewholder/ErrorStateViewHolder.java
+++ app/src/main/java/br/com/alessanderleite/moviesappomdbapi/feature/search/viewholder/ErrorStateViewHolder.java
@@ -1,4 +1,11 @@
 package br.com.alessanderleite.moviesappomdbapi.feature.search.viewholder;
 
-public class ErrorStateViewHolder {
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+public class ErrorStateViewHolder extends RecyclerView.ViewHolder {
+    public ErrorStateViewHolder(@NonNull View itemView) {
+        super(itemView);
+    }
 }
