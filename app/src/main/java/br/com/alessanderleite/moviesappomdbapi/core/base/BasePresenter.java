b3a7628cdda682c951a63b62735ed8acfaf5781f
alessanderleite <alessander.logistica@gmail.com>
2019-04-27 15:50:38 -0300

    code the BasePresenter interface

--- app/src/main/java/br/com/alessanderleite/moviesappomdbapi/core/base/BasePresenter.java
+++ app/src/main/java/br/com/alessanderleite/moviesappomdbapi/core/base/BasePresenter.java
@@ -1,4 +1,11 @@
 package br.com.alessanderleite.moviesappomdbapi.core.base;
 
+import br.com.alessanderleite.moviesappomdbapi.core.api.OMDbApiService;
+
 public interface BasePresenter {
+    void start();
+
+    void stop();
+
+    void loadApi(OMDbApiService service);
 }
