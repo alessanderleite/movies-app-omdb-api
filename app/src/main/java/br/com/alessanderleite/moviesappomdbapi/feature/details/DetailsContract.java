bb11645191ca89f0f0c519db1b21bc892eb13b42
alessanderleite <alessander.logistica@gmail.com>
2019-04-27 18:08:10 -0300

    add the methods inside DetailsContract interface

--- app/src/main/java/br/com/alessanderleite/moviesappomdbapi/feature/details/DetailsContract.java
+++ app/src/main/java/br/com/alessanderleite/moviesappomdbapi/feature/details/DetailsContract.java
@@ -1,4 +1,40 @@
 package br.com.alessanderleite.moviesappomdbapi.feature.details;
 
+import br.com.alessanderleite.moviesappomdbapi.core.base.BasePresenter;
+import br.com.alessanderleite.moviesappomdbapi.core.base.BaseView;
+
 public interface DetailsContract {
+
+    interface View extends BaseView<Presenter> {
+
+        void setPoster(String poster);
+
+        void setTitle(String title);
+
+        void setThumbnail(String poster);
+
+        void setPlot(String plot);
+
+        void setYearAndRuntime(String year, String runtime);
+
+        void setActors(String actors);
+
+        void setDirector(String director);
+
+        void setProduction(String production);
+
+        void setWriters(String writers);
+
+        void setIMDbRating(String imdbRating);
+
+        void showError();
+
+        void showLoading();
+
+        void dismissLoading();
+    }
+
+    interface Presenter extends BasePresenter {
+        void onSearchMovie(String movie);
+    }
 }
