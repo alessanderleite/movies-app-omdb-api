1c431905f51710af1495c4d1a61488272bbb9aec
alessanderleite <alessander.logistica@gmail.com>
2019-04-27 15:55:21 -0300

    code the OMDbApi interface

--- app/src/main/java/br/com/alessanderleite/moviesappomdbapi/core/api/OMDbApi.java
+++ app/src/main/java/br/com/alessanderleite/moviesappomdbapi/core/api/OMDbApi.java
@@ -1,4 +1,16 @@
 package br.com.alessanderleite.moviesappomdbapi.core.api;
 
+import br.com.alessanderleite.moviesappomdbapi.models.Movie;
+import br.com.alessanderleite.moviesappomdbapi.models.SearchResponse;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
 public interface OMDbApi {
+
+    @GET("/")
+    Call<SearchResponse> searchByTitle(@Query("s") String title);
+
+    @GET("/")
+    Call<Movie> searchByOMDbId(@Query("i") String omdbId);
 }
