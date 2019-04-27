44561aac8fd82c30dff9e6f09626d475b636dc22
alessanderleite <alessander.logistica@gmail.com>
2019-04-27 19:23:30 -0300

    code the SearchContract interface

--- app/src/main/java/br/com/alessanderleite/moviesappomdbapi/feature/search/SearchContract.java
+++ app/src/main/java/br/com/alessanderleite/moviesappomdbapi/feature/search/SearchContract.java
@@ -1,4 +1,28 @@
 package br.com.alessanderleite.moviesappomdbapi.feature.search;
 
+import java.util.List;
+
+import br.com.alessanderleite.moviesappomdbapi.core.base.BasePresenter;
+import br.com.alessanderleite.moviesappomdbapi.core.base.BaseView;
+import br.com.alessanderleite.moviesappomdbapi.feature.details.DetailsContract;
+import br.com.alessanderleite.moviesappomdbapi.models.Card;
+
 public interface SearchContract {
+
+    interface View extends BaseView<DetailsContract.Presenter> {
+
+        void showMovies(List<Card> results);
+
+        void cleanSearch();
+
+        void hideKeyboard();
+
+        void showError();
+
+        void showLoading();
+    }
+
+    interface Presenter extends BasePresenter {
+        void onSearchedMovie(String movie);
+    }
 }
