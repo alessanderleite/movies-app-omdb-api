bb3814f9668ec5e6cc56b3e611903d334ed6bcb8
alessanderleite <alessander.logistica@gmail.com>
2019-04-27 19:19:18 -0300

    code the CardMovieViewHolderContract interface

--- app/src/main/java/br/com/alessanderleite/moviesappomdbapi/feature/search/viewholder/CardMovieViewHolderContract.java
+++ app/src/main/java/br/com/alessanderleite/moviesappomdbapi/feature/search/viewholder/CardMovieViewHolderContract.java
@@ -1,4 +1,8 @@
 package br.com.alessanderleite.moviesappomdbapi.feature.search.viewholder;
 
 public interface CardMovieViewHolderContract {
+    void setTitle(String title);
+    void setYear(String year);
+    void setMovieThumbnail(String poster);
+    void resertViews();
 }
